ad15875cfbe29afa9c4310c9bb02463cc6ea03f9
Godzzo <egodzzo@gmail.com>
2020-02-13 19:35:40 +0100

    First working try out

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+ public void Red() {
+        Debug.Log("Change color to RED");
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+    public void Green() {
+        Debug.Log("Change color to GREEN");
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+    public void Blue() {
+        Debug.Log("Change color to BLUE");
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+        // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("ChangeColor START");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
